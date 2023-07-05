a4019fe908e98442f6799045a4d6436c3cbf0b72
Sapas <nandresthio@gmail.com>
2023-07-05 10:08:29 -0500

    Modified solar interface function to recognise whether linux or windows is running

--- cpp_code/functions.cpp
+++ cpp_code/functions.cpp
@@ -134,14 +134,26 @@ double SOLARFunction::callSimulation(VectorXd &inputPoint, double fidelity){
 	VectorXd point = scalePoint(inputPoint);
 	// Need to create a file, save the point to be evaluated, evaluate, then delete the file
 	ofstream pointOutput;
-	pointOutput.open("cpp_code\\solar\\point" + to_string(fileNum_) + ".txt");
+	#if defined(__linux__)
+		pointOutput.open("cpp_code/solar/point" + to_string(fileNum_) + ".txt");
+	#elif _WIN32
+		pointOutput.open("cpp_code\\solar\\point" + to_string(fileNum_) + ".txt");
+	#else
+		printf("Unsure what system is running this code, stopping here!\n");
+		exit(0);
+	#endif
 	// File is now open, can write to it
 	for(int i = 0; i < point.size(); i++){
 		pointOutput << point(i) << " ";
 	}
 	pointOutput << "\n";
 	pointOutput.close();
-	string command = "cpp_code\\solar\\bin\\solar 10 cpp_code\\solar\\point" + to_string(fileNum_) + ".txt -fid=" + to_string(fidelity);
+	string command;
+	#if defined(__linux__)
+		command = "cpp_code/solar/bin/solar 10 cpp_code/solar/point" + to_string(fileNum_) + ".txt -fid=" + to_string(fidelity);	
+	#elif _WIN32
+		command = "cpp_code\\solar\\bin\\solar 10 cpp_code\\solar\\point" + to_string(fileNum_) + ".txt -fid=" + to_string(fidelity);
+	#endif
 	// Got the code to run system and get output from
 	// https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
 	array<char, 128> buffer;
@@ -155,7 +167,13 @@ double SOLARFunction::callSimulation(VectorXd &inputPoint, double fidelity){
     }
 
 	// Still need to extract the output!
-	string filename = "cpp_code\\solar\\point" + to_string(fileNum_) + ".txt";
+	string filename;
+	#if defined(__linux__)
+		filename = "cpp_code/solar/point" + to_string(fileNum_) + ".txt";
+	#elif _WIN32
+		filename = "cpp_code\\solar\\point" + to_string(fileNum_) + ".txt";
+	#endif
+
 	remove(filename.c_str());
 
 	// When the code executes, if a hidden constraint is violated, 1e+20 is returned. Replace this with upper bound on the function output
